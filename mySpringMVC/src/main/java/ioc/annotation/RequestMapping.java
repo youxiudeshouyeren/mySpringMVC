b72028dc2b1a2b2c08163e436ba31383a6102179
sjx <1929724847@qq.com>
2020-11-19 19:51:30 +0800

    mvc完善

--- mySpringMVC/src/main/java/ioc/annotation/RequestMapping.java
+++ mySpringMVC/src/main/java/ioc/annotation/RequestMapping.java
@@ -2,9 +2,12 @@ package ioc.annotation;
 
 import java.lang.annotation.*;
 
+
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE,ElementType.METHOD})//作用在类和方法 仿照springmvc的套路来
 @Documented
 public @interface RequestMapping {
     String value() default "";
+
 }
